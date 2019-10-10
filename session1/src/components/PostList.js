95989dcebc4ca964a3e39519f73030b26e214e58
Yordan Ramchev <dakata911@gmail.com>
2019-10-10 20:13:31 +0300

    add remove post feature

--- session1/src/components/PostList.js
+++ session1/src/components/PostList.js
@@ -7,14 +7,13 @@ class PostList extends Component {
     posts: this.props.posts
   };
 
-  hidePost(id) {
+  hidePost = (id) => {
+    let postData = this.state.posts;
+    postData.splice(id, 1)
     
-    console.log(this.state.posts)
-    // let updatedPosts = this.state.posts.slice(id, 1);
-    
-    // this.setState({
-    //   posts: updatedPosts
-    // });
+    this.setState({
+      posts: postData
+    });
   }
 
   render() {
@@ -22,19 +21,25 @@ class PostList extends Component {
       <section className="post-list">
         <div className="container">
           <h1 className="title">{this.state.sectionTitle}</h1>
-          <div className="columns">
-            {this.state.posts.map(post => (
-              <div className="column" key={post.id}>
-                <PostListItem
-                  id={post.id}
-                  title={post.title}
-                  tagline={post.tagline}
-                  votes={post.votes}
-                  hidePost={this.hidePost}
-                />
-              </div>
-            ))}
-          </div>
+          {this.state.posts.length !== 0 ? (
+            <div className="columns">
+              {this.state.posts.map((post, index) => (
+                <div className="column" key={post.id}>
+                  <PostListItem
+                    id={index}
+                    title={post.title}
+                    tagline={post.tagline}
+                    votes={post.votes}
+                    hidePost={this.hidePost}
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="columns">
+              No Posts!
+            </div>
+          )}
         </div>
       </section>
     );
