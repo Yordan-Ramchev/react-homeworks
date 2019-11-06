2b95806d4689b474059b89840ba876065abd3728
Dakata <dakata911@gmail.com>
2019-11-07 01:24:25 +0200

    wip

--- session3/src/App.tsx
+++ session3/src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React from "react";
 import "bulma/css/bulma.css";
-import './App.css';
+import "./App.css";
 import Cart from "./components/Cart";
+import FormTest from "./components/FormTest";
+import { Formik, FormikProps, Form, Field, FieldProps } from "formik";
+
+interface MyFormValues {
+  firstName: string;
+}
 
 const App: React.FC = () => {
+  const initialValues: MyFormValues = { firstName: "" };
   return (
-    <div className="App">
-      <Cart />
-    </div>
+    <>
+      <div className="App">
+        <Cart />
+      </div>
+      <div className="container">
+        <FormTest />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
